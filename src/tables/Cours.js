e10d821da25d8d245a7e648704d039b8e8bd765f
David Morais Pereira <david.morais-pereira@etu.univ-amu.fr>
2023-09-19 17:03:24 +0200

    Creation des class des tables de la bdd en js

---
+++ src/tables/Cours.js
@@ -0,0 +1,22 @@
+class Cours {
+    constructor(id, nomProf, nom) {
+      this.id = id;
+      this.nomProf = nomProf;
+      this.nom = nom;
+    }
+  
+    getId() {
+      return this.id;
+    }
+  
+    getNomProf() {
+      return this.nomProf;
+    }
+  
+    getNom() {
+      return this.nom;
+    }
+
+    getTiming() {}
+  }
+  
